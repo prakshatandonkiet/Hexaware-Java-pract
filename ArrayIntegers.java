d02da620a5acedd5931b835602102f48b6832c90
prakshatandonkiet <75250776+prakshatandonkiet@users.noreply.github.com>
2022-04-07 11:18:22 +0530

    Add files via upload

---
+++ ArrayIntegers.java
@@ -0,0 +1,11 @@
+public class ArrayIntegers {
+    public static void main(String[] args) {
+        int arr[] = new int[3];
+            arr[0] = Integer.parseInt(args[0]);            
+            arr[1] = Integer.parseInt(args[1]);            
+            arr[2] = Integer.parseInt(args[2]); 
+         
+            System.out.println("Values are "+ arr[0]+" "+arr[1]+ " "+arr[2]);
+
+    }
+}
